1f1bd7d1b25ee7a967aae2c760fc28e8b476bcef
gonmace <magoreal38@gmail.com>
2024-10-14 14:24:55 -0400

    frame

--- config/base.py
+++ config/base.py
@@ -116,3 +116,5 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+X_FRAME_OPTIONS = 'ALLOW-FROM https://cloud.redlinegs.com'
